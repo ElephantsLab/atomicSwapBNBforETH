2c124a3a7c8ced6ae81a06412d461470c625b07f
Andrii Komar <andriikomar@elephantslab.com>
2020-11-17 16:53:17 +0200

    multySig contract ERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var L2 = artifacts.require("../contracts/erc20Manage.sol");
+var L2 = artifacts.require("../contracts/erc20MultySig.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(L2);
